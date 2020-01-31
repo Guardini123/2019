74235bf1475bdd3d2f80049e698d18d3b50159b4
Guardini123 <44317871+Guardini123@users.noreply.github.com>
2020-01-31 15:36:09 +0300

    Delete ViewClass.java

--- WebParcing/src/java/ViewClass.java
+++
@@ -1,7 +0,0 @@
-package com.ggs.webparsing3.model.model;
-
-public class ViewClass {
-    public static void UpdateResult(float result){
-        MainActivity.result.setText(Float.toString(result));
-    }
-}
