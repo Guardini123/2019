4506c732767d80963e002f3d4614687950c57722
Guardini123 <44317871+Guardini123@users.noreply.github.com>
2020-01-31 15:34:36 +0300

    Add files via upload

---
+++ WebParcing/src/java/ViewClass.java
@@ -0,0 +1,7 @@
+package com.ggs.webparsing3.model.model;
+
+public class ViewClass {
+    public static void UpdateResult(float result){
+        MainActivity.result.setText(Float.toString(result));
+    }
+}
